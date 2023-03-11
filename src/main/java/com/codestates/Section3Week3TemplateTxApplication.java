040906892f8b976dbc7d7741f0af8d5d2d89da87
chaeyeon0130 <k84292330@gmail.com>
2023-03-11 15:41:05 +0900

    init

--- src/main/java/com/codestates/Section3Week3TemplateTxApplication.java
+++ src/main/java/com/codestates/Section3Week3TemplateTxApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Section3Week3TemplateTxApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(Section3Week3TemplateTxApplication.class, args);
 	}
 
