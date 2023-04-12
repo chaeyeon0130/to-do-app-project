09e28ffd0ea5fd03f4fcf7f4026a9fe6d59964b4
chaeyeon0130 <k84292330@gmail.com>
2023-04-12 17:30:26 +0900

    init: spring boot 초기 세팅

---
+++ src/test/java/com/codestates/todoappproject/ToDoAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todoappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
